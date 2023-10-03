16793280785cbd3f11c589f5c974ad2567c221b9
kellynge1ywa <120782457+kellynge1ywa@users.noreply.github.com>
2023-10-03 14:30:03 +0300

    c-sharp for loop

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace For_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = GetPow(3, 4);
+            Console.WriteLine(result);
+            for (int i=0; i <=4; i+=2)
+            {
+                for (int j=0; j <=4; j+=2)
+                    for(int k=0; k <= 4; k+=2)
+                    {
+                        Console.WriteLine($"{i},{j},{k}");
+
+                    }
+                
+            }
+
+            string[] fruits = { "Mangoes","Apples","Citrus","Avocado","Grapes","Passion" };
+
+            for (int fruit=0; fruit < fruits.Length; fruit++)
+            {
+                Console.WriteLine(fruits[fruit]);
+            }
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int value = 1;
+
+            for( int i=0; i <powNum; i++)
+            {
+                value = value * baseNum;
+            }
+
+            return value;
+        }
+    }
+}
